1035a09f9865567fcb5b3330755c72d922224ffb
Adhitya <Adhitya@LAPTOP-5I008H6Q>
2019-10-22 08:52:40 +0530

    Adding Greatest of Numbers Program

---
+++ src/main/java/com/fisrtset/javaprog/GreatestOfAllNumbers.java
@@ -0,0 +1,53 @@
+package com.fisrtset.javaprog;
+
+import static org.testng.Assert.ARRAY_MISMATCH_TEMPLATE;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+
+
+public class GreatestOfAllNumbers {
+	
+	@Test(enabled=true)
+	public void greatestOfNumsWithList()
+	{
+		int[] arr = {5,4,44,88,56,1000,2,1,0,6};
+		String str;
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for(int array : arr)
+		{
+			list.add(array);
+		}
+		Collections.sort(list);
+		Collections.reverse(list);
+		System.out.println("Greatest num using list:" +list.get(0));
+	}
+	
+	@Test
+	public void greatestOfNums()
+	{
+		int[] arr = {5,4,44,88,56,1000,2,1,0,6};
+		int max = arr[0];
+		for (int i=1; i<arr.length; i++)
+		{
+			if (arr[i]>max)
+			{
+				max = arr[i];
+			}
+		}
+		System.out.println("Greatest num:" +max);
+	}
+	@Test
+	public void greatestOfNumsUsingStream()
+	{
+		int[] arr = {5,4,44,88,56,1000,2,1,0,6};
+		int max = Arrays.stream(arr).max().getAsInt();
+		System.out.println("Greatest num using Stream:" +max);
+	}
+
+}
